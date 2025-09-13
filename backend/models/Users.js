0f94f9474af102ed4ba9f85ce557d3a7c617c0ca
EvradeMarc <jasonmbezele@gmail.com>
2025-09-13 23:33:31 +0200

    Modification du schéma user

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -10,6 +10,12 @@ const userSchema = new mongoose.Schema(
       type: String, 
       required: true
     },
+    phonenumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
